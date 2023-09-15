b3fd16db19db85c0677e84afbffdf0d027722e19
catarium <tartarxan@gmail.com>
2023-09-15 14:35:17 +0300

    kinda models

---
+++ bot/db/models/classroom.py
@@ -0,0 +1,13 @@
+from typing import List
+
+from sqlalchemy import Column, Integer, Text
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+
+from bot.db.models.base import Base
+
+
+class Classroom(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    teacher: Mapped['Teacher'] = relationship(back_populates="classes")
+    students: Mapped[List['Student']] = relationship(back_populates='class_',
+                                                     cascade='all, delete_orphan')
--- bot/db/models/user.py
+++ bot/db/models/user.py
@@ -1,31 +1,33 @@
-from sqlalchemy import Column, Integer, Text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import DeclarativeBase
+from typing import List
 
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-class Base(DeclarativeBase):
-    pass
+from bot.db.models.base import Base
 
 
 class User(Base):
     __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    lastname = Column(Text)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    telgram_id: Mapped[str]
+    name: Mapped[str]
+    lastname: Mapped[str]
 
     __mapper_args__ = {
-        "polymorphic_on": True,
+        "polymorphic_on": "type",
         "polymorphic_identity": "user"
     }
 
 
 class Teacher(User):
+    classes: Mapped[List['Class']] = relationship(back_populates='teacher',
+                                                     cascade='all, delete_orphan')
     __mapper_args__ = {
         "polymorphic_identity": "teacher"
     }
 
 
 class Student(User):
+    class_: Mapped['Class'] = relationship(back_populates='')
     __mapper_args__ = {
         "polymorphic_identity": "student"
     }
