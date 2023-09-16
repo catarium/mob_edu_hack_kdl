28f0ab2f06367b2a4b71691608a46e1b3d636d12
WeAllLiveIn <132277255+WeAllLiveIn@users.noreply.github.com>
2023-09-16 10:26:05 +0300

    _

--- bot/handlers/keyboards/user_type.py
+++ bot/handlers/keyboards/user_type.py
@@ -3,7 +3,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def get_user_type_keyboard():
     buttons = [
-        [InlineKeyboardButton(text="Уичтель", callback_data="type_teacher")],
+        [InlineKeyboardButton(text="Учитель", callback_data="type_teacher")],
         [InlineKeyboardButton(text='Ученик', callback_data="type_student")]
     ]
     keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
