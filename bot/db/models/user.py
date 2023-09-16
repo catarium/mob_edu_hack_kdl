c52a0adfb94453e6930bf6a5c43d351afeffb6ef
WeAllLiveIn <132277255+WeAllLiveIn@users.noreply.github.com>
2023-09-16 15:14:26 +0300

    New RegistrSys

--- bot/db/models/user.py
+++ bot/db/models/user.py
@@ -1,38 +1,40 @@
 from typing import List, Optional
 
-from sqlalchemy import ForeignKey, create_engine, Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.ext.declarative import declarative_base
 
 from bot.db.models.base import Base
 
 
+class User(Base):
+    __tablename__ = 'user'
 
-class Teacher:
-    __tablename__ = 'teacher'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    telegram_id: Mapped[str] = mapped_column(unique=True)
+    name: Mapped[str]
+    lastname: Mapped[str]
+    type: Mapped[str]
 
-    teacher_id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String, nullable=False)
-    class_id = relationship('Class_id', back_populates='classrooms')
+    __mapper_args__ = {
+        "polymorphic_on": 'type',
+        "polymorphic_identity": "user"
+    }
 
 
-class Student:
-    __tablename__ = 'student'
+class Teacher(User):
+    classes: Mapped[List['Classroom']] = relationship(back_populates='teacher',
+                                                     cascade='all, delete-orphan',
+                                                      foreign_keys='Classroom.teacher_id')
+    __mapper_args__ = {
+        "polymorphic_identity": "teacher"
+    }
 
-    stud_id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String, nullable=False)
-    marks = Column(String)
 
-class ClassRoom:
-    __tablename__ = 'classrooms'
-
-    class_id = Column(Integer, primary_key=True, nullable=False)
-    class_name = Column(String)
-    way_id = relationship('Way_id', back_populates='way')
-
-class Way:
-    __tablename__ = 'way'
-
-    Way_id = Column(Integer, primary_key=True, nullable=False)
-    way_name = Column(String)
-
+class Student(User):
+    rating: Mapped[int] = mapped_column(default=0)
+    classroom_id: Mapped[Optional[int]] = mapped_column(ForeignKey('classroom.id'))
+    classroom: Mapped[Optional['Classroom']] = relationship(back_populates='',
+                                                        foreign_keys=[classroom_id])
+    __mapper_args__ = {
+        "polymorphic_identity": "student"
+    }
--- bot/handlers/auth.py
+++ bot/handlers/auth.py
@@ -1,7 +1,8 @@
+from sqlalchemy.orm import Session, sessionmaker
 from aiogram import F
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from bot.db.crud.user import UserCRUD, TeacherCRUD, StudentCRUD
 from bot.db.models.user import *
 from bot.handlers.keyboards.user_type import get_user_type_keyboard
@@ -9,6 +10,8 @@ from bot.handlers.states.auth import AuthGroup
 from bot.misc import dp, session_maker
 from bot.db.models.user import Teacher, Student
 
+session = session_maker(bind="engine")
+session = Session()
 button1 = InlineKeyboardButton(text='Учитель')
 button2 = InlineKeyboardButton(text='Ученик')
 keyboard1 = InlineKeyboardMarkup(inline_keyboard=[[button1], [button2]])
@@ -16,6 +19,34 @@ keyboard1 = InlineKeyboardMarkup(inline_keyboard=[[button1], [button2]])
 async def reg(message: Message):
     await message.answer('Какой ваш статус', reply_markup=keyboard1)
 
+
+@dp.message(F.text('Ученик'))
+async def teach(message: Message):
+    id = message.from_user.id
+    # сюда писать ввод id и статуса ученика в бд
+    await message.answer('Введите Фамилию и имя через пробел, используя /namest <Имя> <Фамилия>')
+
 @dp.message(F.text('Учитель'))
 async def teach(message: Message):
-    await async
+    id = message.from_user.id
+    # сюда писать ввод id и статуса учителя в бд
+    await message.answer('Введите Фамилию и имя через пробел, используя /namet <Имя> <Фамилия>')
+
+
+@dp.message(Command(commands=['namet']))
+async def name(message: Message):
+    name = message.text.split()
+    if len(name) < 3:
+        await message.answer(f'Неправильный формат ввода')
+    else:
+        # сюда писать ввод id и статуса учителя в бд
+        await message.answer(f'Вы успешно зарегистрированы под именем {name[1:]}')
+
+@dp.message(Command(commands=['namest']))
+async def name(message: Message):
+    name = message.text.split()
+    if len(name) < 3:
+        await message.answer(f'Неправильный формат ввода')
+    else:
+        # сюда писать ввод id и статуса студня в бд
+        await message.answer(f'Вы успешно зарегистрированы под именем {name[1:]}')
